14b6ce45bba383f9687c094f8ac43c1f6ad732bf
KhiQyam <115384376+marianogalauran23@users.noreply.github.com>
2025-03-26 07:00:50 +0800

    Update firebaseConfig.ts

--- backEnd/firebaseConfig.ts
+++ backEnd/firebaseConfig.ts
@@ -9,17 +9,7 @@ import { getFirestore } from "firebase/firestore";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAeXrRBiBzEM5DmtSDVZ3FTojKO443h4RQ",
-  authDomain: "tcu-planorama.firebaseapp.com",
-  databaseURL: "https://tcu-planorama-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "tcu-planorama",
-  storageBucket: "tcu-planorama.firebasestorage.app",
-  messagingSenderId: "164200453353",
-  appId: "1:164200453353:web:25c3bb8cfafb0f8cf98c78",
-  measurementId: "G-MTBVVEVN87"
-};
-// Initialize Firebase app
+//e Firebase app
 const app = initializeApp(firebaseConfig);
 
 // ✅ Initialize Firebase Auth
